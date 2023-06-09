b4c4102db3aeb9bf5605206ec31373ca7a935cb1
Nachtalb <na@nachtalb.io>
2023-06-09 11:57:23 +0200

    Configure foot

--- install.sh
+++ install.sh
@@ -28,24 +28,27 @@ go get github.com/sirupsen/logrus
 go get github.com/Difrex/gosway/ipc
 
 printf "\n${GREEN}${BOLD}Symlinking default config.${RESET}\n"
-ln  -s  $HOME/.config/sway/conf-available/workspaces.conf           $HOME/.config/sway/conf-enabled/00-workspaces.conf
-ln  -s  $HOME/.config/sway/conf-available/autostart.conf            $HOME/.config/sway/conf-enabled/01-autostart.conf
-ln  -s  $HOME/.config/sway/conf-available/fonts.conf                $HOME/.config/sway/conf-enabled/02-fonts.conf
-ln  -s  $HOME/.config/sway/conf-available/keybindings.conf          $HOME/.config/sway/conf-enabled/03-keybindings.conf
-ln  -s  $HOME/.config/sway/conf-available/layout.conf               $HOME/.config/sway/conf-enabled/04-key-layout.conf
-ln  -s  $HOME/.config/sway/conf-available/terminal-dark.conf        $HOME/.config/sway/conf-enabled/05-terminal.conf
-ln  -s  $HOME/.config/sway/conf-available/colors-dark.conf          $HOME/.config/sway/conf-enabled/20-colors.conf
-ln  -s  $HOME/.config/sway/conf-available/gaps.conf                 $HOME/.config/sway/conf-enabled/30-gaps.conf
-ln  -s  $HOME/.config/sway/conf-available/idle.conf                 $HOME/.config/sway/conf-enabled/40-idle.conf
-ln  -s  $HOME/.config/sway/conf-available/screenshots.conf          $HOME/.config/sway/conf-enabled/50-screenshots.conf
-ln  -s  $HOME/.config/sway/conf-available/wallpapers.conf           $HOME/.config/sway/conf-enabled/80-wallpapers.conf
-ln  -s  $HOME/.config/sway/conf-available/colors-dark.conf          $HOME/.config/sway/conf-enabled/colors-dark.conf
-ln  -s  $HOME/.config/sway/conf-available/mako-dark.conf            $HOME/.config/sway/conf-enabled/mako-dark.conf
-ln  -s  $HOME/.config/sway/conf-available/terminal-dark.conf        $HOME/.config/sway/conf-enabled/terminal-dark.conf
-ln  -s  $HOME/.config/sway/conf-available/transparent-windows.conf  $HOME/.config/sway/conf-enabled/transparent-windows.conf
 
-ln  -s  $HOME/.config/sway/waybar-available/config          $HOME/.config/sway/waybar-enabled/config
-ln  -s  $HOME/.config/sway/waybar-available/style-dark.css  $HOME/.config/sway/waybar-enabled/style.css
+# sway
+ln  -s  ~/.config/sway/conf-available/workspaces.conf           ~/.config/sway/conf-enabled/00-workspaces.conf
+ln  -s  ~/.config/sway/conf-available/autostart.conf            ~/.config/sway/conf-enabled/01-autostart.conf
+ln  -s  ~/.config/sway/conf-available/fonts.conf                ~/.config/sway/conf-enabled/02-fonts.conf
+ln  -s  ~/.config/sway/conf-available/keybindings.conf          ~/.config/sway/conf-enabled/03-keybindings.conf
+ln  -s  ~/.config/sway/conf-available/layout.conf               ~/.config/sway/conf-enabled/04-key-layout.conf
+ln  -s  ~/.config/sway/conf-available/terminal-dark.conf        ~/.config/sway/conf-enabled/05-terminal.conf
+ln  -s  ~/.config/sway/conf-available/colors-dark.conf          ~/.config/sway/conf-enabled/20-colors.conf
+ln  -s  ~/.config/sway/conf-available/gaps.conf                 ~/.config/sway/conf-enabled/30-gaps.conf
+ln  -s  ~/.config/sway/conf-available/idle.conf                 ~/.config/sway/conf-enabled/40-idle.conf
+ln  -s  ~/.config/sway/conf-available/screenshots.conf          ~/.config/sway/conf-enabled/50-screenshots.conf
+ln  -s  ~/.config/sway/conf-available/wallpapers.conf           ~/.config/sway/conf-enabled/80-wallpapers.conf
+ln  -s  ~/.config/sway/conf-available/colors-dark.conf          ~/.config/sway/conf-enabled/colors-dark.conf
+ln  -s  ~/.config/sway/conf-available/mako-dark.conf            ~/.config/sway/conf-enabled/mako-dark.conf
+ln  -s  ~/.config/sway/conf-available/terminal-dark.conf        ~/.config/sway/conf-enabled/terminal-dark.conf
+ln  -s  ~/.config/sway/conf-available/transparent-windows.conf  ~/.config/sway/conf-enabled/transparent-windows.conf
+
+# waybar
+ln  -s  ~/.config/sway/waybar-available/config          ~/.config/sway/waybar-enabled/config
+ln  -s  ~/.config/sway/waybar-available/style-dark.css  ~/.config/sway/waybar-enabled/style.css
 
 printf "\n${GREEN}${BOLD}Building GO components.${RESET}\n"
 cd scripts && go build -o ~/.local/bin/tr_in transparent_inctive.go
@@ -58,6 +61,10 @@ default_linemode devicons
 
 # Image preview
 set preview_images true
-set preview_images_method ueberzug" >> $HOME/.config/ranger/rc.conf`
+set preview_images_method ueberzug" >> ~/.config/ranger/rc.conf`
+
+printf "\n${GREEN}${BOLD}Configure 'foot' (terminal).${RESET}\n"
+mkdir ~/.config/foot -p
+ln -s ~/.config/sway/foot/foot.ini ~/.config/foot/
 
 printf "\n${GREEN}${BOLD}Done installing, you have to relogin now.${RESET}\n"
