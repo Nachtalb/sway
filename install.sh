f2a72b1c69c53730dffc5c20f178fc77c1a3ea24
Nachtalb <na@nachtalb.io>
2023-06-09 11:57:22 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+go get github.com/sirupsen/logrus
+go get github.com/Difrex/gosway/ipc
+paru -Sy sway swaybg swayidle swaylock swaylock-blur-bin go rofi mako pm grim slurp python-pywal
